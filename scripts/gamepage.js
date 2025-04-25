e8f9e57c6d8c85af6bc9d877dafc2ad4c9f1ea1d
maji <150906506+maji-git@users.noreply.github.com>
2025-04-25 13:07:39 +0700

    Add useful tooltips

--- scripts/gamepage.js
+++ scripts/gamepage.js
@@ -31,31 +31,37 @@ const delay = (delayInms) => {
 
 async function main() {
     await delay(1000)
+    console.log("delay passed")
     await waitForElm(".game-stat-container")
+    console.log("stat found")
 
     const gameStatContainer = document.querySelector(".game-stat-container")
+
+    console.log(gameStatContainer)
 
     chrome.runtime.sendMessage({
         req: "check_support",
         gameId
     }, (response) => {
         let vrSupportText = "No data"
+        let vrSupportSubtext = "This game either does not have VR support or hasn't been checked for VR support yet."
 
         if (response == "native") {
             vrSupportText = "Supported"
+            vrSupportSubtext = "This game supports VR out of the box."
         }
         if (response == "nexus") {
             vrSupportText = "Nexus VR"
+            vrSupportSubtext = "This game supports VR via Nexus VR Character Model"
         }
         if (response == "avatar-gestures") {
             vrSupportText = "Avatar Gestures"
+            vrSupportSubtext = "This game supports VR via Avatar Gestures"
         }
 
-        if (response) {
-            gameStatContainer.innerHTML += `
-            <li class="game-stat"><p class="text-label text-overflow font-caption-header">VR</p><p class="text-lead font-caption-body">${vrSupportText}</p></li>
+        gameStatContainer.innerHTML += `
+            <li class="game-stat"><p class="text-label text-overflow font-caption-header">VR</p><p class="text-lead font-caption-body" title="${vrSupportSubtext}">${vrSupportText}</p></li>
             `
-        }
     })
 }
 
